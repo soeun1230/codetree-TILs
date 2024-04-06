63439540c6db868119f464e33d5ef4775f3658ed
soeun1230 <113100154+soeun1230@users.noreply.github.com>
2024-04-06 17:07:38 +0900

    Title: 블럭의 총 둘레, Time: 177ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 240406/블럭의 총 둘레/total-perimeter-of-blocks.java
+++ 240406/블럭의 총 둘레/total-perimeter-of-blocks.java
@@ -6,12 +6,7 @@ public class Main {
     public static int N;
     public static int[][]Map;
     public static int[][]visited;
-    public static int bigRow=0;
-    public static int bigCol=0;
-    public static int smallRow=100;
-    public static int smallCol=100;
-    // public static int[] dx = {0,0,-1,-1,-1,1,1,1};
-    // public static int[] dy = {1,-1,1,-1,0,1,-1,0};
+    public static int sum=0;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
@@ -26,22 +21,47 @@ public class Main {
             int col = Integer.parseInt(input[1])-1;
             Map[row][col]=1;
 
-            if(row<smallRow) smallRow=row;
-            if(row+1>bigRow) bigRow=row+1;
-            if(col<smallCol) smallCol=col;
-            if(col+1>bigCol) bigCol=col+1;
             if(i==0){sr=row; sc=col;}
         }
 
-        int sum = (bigRow-smallRow)*2 + (bigCol-smallCol)*2;
+        for(int i=0;i<100;i++){
+            int ch = 0;
+            for(int j=0;j<100;j++){
+                if(Map[i][j]==1) ch=1;
+                if(Map[i][j]==0 && visited[i][j]==0){
+                    if(ch==1) break;
+                    dfs(i,j);
+                }
+            }
+        }
+
         System.out.println(sum);
-        //dfs(0,sr,sc);
 
     }
-    // public static void dfs(int depth, int nr, int nc){
-    //     if(depth==N){
-            
-    //     }
-    //     for(int i=0;i<)
-    // }
+    public static void dfs(int nr, int nc){
+        if(nr>=100 || nr<0 || nc>=100 || nc<0) return;
+        if(Map[nr][nc]==1 || visited[nr][nc]==1) return;
+
+        visited[nr][nc]=1;
+
+        int[][] direction = {{0,1},{0,-1},{1,0},{-1,0}};
+        for (int[] d : direction) {
+            int nx = nr + d[0];
+            int ny = nc + d[1];
+            if (nx < 0 || nx >= 100 || ny < 0 || ny >= 100) continue;
+
+            if(Map[nx][ny] == 1) {
+                sum++;
+            }
+        }
+
+        for(int[] d : direction){
+            int nx = nr + d[0];
+            int ny = nc + d[1];
+            if (nx >= 0 && nx < 100 && ny >= 0 && ny < 100 && Map[nx][ny] == 0) {
+                dfs(nx, ny);
+            }
+        }
+
+    }
 }
