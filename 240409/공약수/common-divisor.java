1cdedd20b7ac18829afad792bb97a5942f00f0fb
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-04-09 23:28:11 +0900

    Title: 공약수, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240409/공약수/common-divisor.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+
+        if(a==2){
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+            if(b<c){
+                for(int i=1;i<=b;i++){
+                    if(b%i==0 && c%i==0){
+                        System.out.println(i);
+                    }
+                }
+            }
+            else{
+                for(int i=1;i<=c;i++){
+                    if(b%i==0 && c%i==0){
+                        System.out.println(i);
+                    }
+                }
+            }
+        }
+        if(a==3){
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+            int d = scanner.nextInt();
+
+            int k = Math.min(b,c);
+            int t = Math.min(k,d);
+
+            for(int i=1;i<t;i++){
+                if(b%i==0 && c%i==0 && d%i==0){
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
