b563cdbe1eb1db2726961ed524312850bebe4589
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-04-10 19:44:00 +0900

    Title: 최소 최대, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240410/최소 최대/min-max.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int small = Integer.MAX_VALUE;
+        int big = 0;
+
+        int n = scanner.nextInt();
+
+        for(int i=0;i<n;i++){
+            int now = scanner.nextInt();
+            small = Math.min(small,now);
+            big = Math.max(small,now);
+        }
+        System.out.printf("%d %d",small,big);
+    }
+}
