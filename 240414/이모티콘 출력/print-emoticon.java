52aaf703dc80021635d8ac51e6d2e4a0293152f3
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-04-14 18:56:32 +0900

    Title: 이모티콘 출력, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240414/이모티콘 출력/print-emoticon.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("(*´∪`)");
+        sb.append("\n");
+        sb.append("(^・ω・^)");
+        sb.append("\n");
+        sb.append("(=^･^=)");
+
+        System.out.print(sb.toString());
+    }
+}
