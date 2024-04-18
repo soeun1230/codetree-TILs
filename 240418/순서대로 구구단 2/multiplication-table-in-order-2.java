8f5066b2effcb022e1bc896f015385fe0a989085
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-04-18 13:56:26 +0900

    Title: 순서대로 구구단 2, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240418/순서대로 구구단 2/multiplication-table-in-order-2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if(a<b){
+            for(int i=a;i<=b;i++){
+                for(int j=1;j<=9;j++){
+                    System.out.printf("%d * %d = %d  ",i,j,i*j);
+                    if(j%3==0) System.out.println();
+                }
+            }
+        }
+        else{
+            for(int i=a;i>=b;i--){
+                for(int j=1;j<=9;j++){
+                    System.out.printf("%d * %d = %d  ",i,j,i*j);
+                    if(j%3==0) System.out.println();
+                }
+                System.out.println();
+            }
+        }
+    }
+}
