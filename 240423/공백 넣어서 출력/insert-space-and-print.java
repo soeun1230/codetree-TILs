5641fcbe3e9bfe8a2d4940108eca1af9dc5a6ff4
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-04-23 07:03:09 +0900

    Title: 공백 넣어서 출력, Time: 110ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240423/공백 넣어서 출력/insert-space-and-print.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("%5s %16s %13s","score","subject","name");
+        System.out.println();
+        System.out.println();
+        System.out.printf("%5s %16s %13s","100","math","sam");
+        System.out.println();
+        System.out.printf("%5s %16s %13s","88","science","paul");
+        System.out.println();
+        System.out.printf("%5s %16s %13s","96","language","jack");
+    }
+}
