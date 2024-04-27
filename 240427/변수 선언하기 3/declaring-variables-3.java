0dadffd02501e6df3765de292824ac4a03e64042
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-04-27 12:12:13 +0900

    Title: 변수 선언하기 3, Time: 84ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240427/변수 선언하기 3/declaring-variables-3.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a =3;
+        int b =6;
+        int c =9;
+        System.out.printf("%d + %d = %d",a,b,c);
+    }
+}
