daa4259749527acf745b55c417c65b687a8fc356
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-05-02 21:27:06 +0900

    Title: 단위 변환하기, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240502/단위 변환하기/change-unit.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 2.80;
+        double b = 128.40;
+
+        System.out.printf("%.2f mi = %.2f\n",a,a*160934.4);
+        System.out.printf("%.2f ft = %.2f",b,b*30.48);
+    }
+}
