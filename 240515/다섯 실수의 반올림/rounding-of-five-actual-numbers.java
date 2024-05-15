e2b5dffd0760650fa964b8774b2ebcf126985f89
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-05-15 21:28:43 +0900

    Title: 다섯 실수의 반올림, Time: 162ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240515/다섯 실수의 반올림/rounding-of-five-actual-numbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        double d = scanner.nextDouble();
+        double e = scanner.nextDouble();
+
+        System.out.printf("%.3f\n",a);
+        System.out.printf("%.3f\n",b);
+        System.out.printf("%.3f\n",c);
+        System.out.printf("%.3f\n",d);
+        System.out.printf("%.3f\n",e);
+
+    }
+}
