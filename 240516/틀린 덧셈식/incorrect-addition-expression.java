6037415d9ed80e9e9351177b19f8f24f3479e246
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-05-16 22:21:42 +0900

    Title: 틀린 덧셈식, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240516/틀린 덧셈식/incorrect-addition-expression.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a =123;
+        int b =43;
+        int c=80;
+        System.out.printf("%d + %d = %d",a,b,c);
+    }
+}
